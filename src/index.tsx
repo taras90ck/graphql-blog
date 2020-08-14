20e3db082beecf7e4dd4f4216928499efd91f36e
Taras Yemets <yemets.taras@gmail.com>
2020-08-14 14:51:05 +0300

    create apollo client and wrap App with apollo provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const client = new ApolloClient({
+    uri: 'https://graphqlzero.almansi.me/api',
+    cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
